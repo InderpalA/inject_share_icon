37933b3f8f17f8ecd351aac99a6eeb2e94533f5f
InderpalA <107916729+InderpalA@users.noreply.github.com>
2024-06-13 12:26:39 +0100

    Update share_icon.js

--- share_icon.js
+++ share_icon.js
@@ -5,7 +5,7 @@ function createTelegram(position) {
 
     const element = shareParentElements[position];
 
-    // Create the Button for Telegram
+    // Create the <a> for Telegram
     let mobileTelegramIcon = document.createElement("a");
     mobileTelegramIcon.href = `https://t.me/share/url?url=${url}`;
     mobileTelegramIcon.setAttribute('data-track-share', 'Telegram');
@@ -14,14 +14,14 @@ function createTelegram(position) {
     mobileTelegramIcon.setAttribute('aria-label', 'Share on Telegram');
 
     // Create the SVG element
-    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
     svg.setAttribute("height", "15");
     svg.setAttribute("width", "17.5");
     svg.setAttribute("viewBox", "0 0 448 512");
 
     // Create the path element and set its attributes
-    var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path.setAttribute("fill", "white");
     path.setAttribute("d", "M446.7 98.6l-67.6 318.8c-5.1 22.5-18.4 28.1-37.3 17.5l-103-75.9-49.7 47.8c-5.5 5.5-10.1 10.1-20.7 10.1l7.4-104.9 190.9-172.5c8.3-7.4-1.8-11.5-12.9-4.1L117.8 284 16.2 252.2c-22.1-6.9-22.5-22.1 4.6-32.7L418.2 66.4c18.4-6.9 34.5 4.1 28.5 32.2z");
 
